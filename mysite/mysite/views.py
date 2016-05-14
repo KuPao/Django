a8a86e64a7d1ede5b29f9222abada6ad04935516
KuPao <B10430029@mail.ntust.edu.tw>
2016-05-15 01:16:16 +0800

    new

---
+++ mysite/mysite/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django import template
+from django.shortcuts import render_to_response
+
+def here(request):
+	return HttpResponse('Hello World!')
+
+
+def add(request, a, b):
+	s = int(a) + int(b)
+	return HttpResponse(str(s))
+
+def math(request, a, b):
+	a = int(a)
+	b = int(b)
+	s = a + b
+	d = a - b
+	p = a * b
+	q = a / b
+	html = '<html>sum={s}<br>dif={d}<br>pro={p}<br>quo={q}</html>'.format(s=s, d=d, p=p, q=q)
+	return HttpResponse(html)
+
+def mathnew(request, a, b):
+	a = int(a)
+	b = int(b)
+	s = a + b
+	d = a - b
+	p = a * b
+	q = a / b
+	return render_to_response('math.html',{'s':s, 'd':d, 'p':p, 'q':q})
+
+def BS(request):
+	return render_to_response('BS.html')
